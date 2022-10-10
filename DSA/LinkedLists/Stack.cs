05765b31335f95a8a3344b58d0e3d1fdaa41e7e5
YasserElmor <elmor.yasser@gmail.com>
2022-10-10 12:49:13 +0200

    Added Stack implementation and unit tests

---
+++ DSA/LinkedLists/Stack.cs
@@ -0,0 +1,70 @@
+﻿
+namespace LinkedLists
+{
+    public class Stack<T>
+    {
+        public int length { get; set; }
+
+        protected Node<T>? head;
+
+        public Stack()
+        {
+            head = null;
+            length = 0;
+        }
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+
+            // empty stack? set the head to the node holding the passed item
+
+            if (length == 0)
+            {
+                head = node;
+            }
+
+            // head is not null? set the new node as the new head and set it to point to the old head
+            else
+            {
+                node!.next = head;
+                head = node;
+            }
+
+            length++;
+        }
+
+        public T? Pop()
+        {
+            // empty stack? return null
+            if (length == 0)
+                return default;
+
+            length--;
+            var prevHead = head;
+
+            // only one node? set head to null
+            if (length == 0)
+            {
+                head = null;
+            }
+
+            // more than one node? set the head to the next node, and free the popped node
+            else
+            {
+                head = head!.next;
+                prevHead.next = null;
+            }
+
+            return prevHead!.value;
+        }
+
+        public T? Peek()
+        {
+            if (length == 0)
+                return default;
+
+            return head!.value;
+        }
+    }
+}
---
+++ DSATests/LinkedLists/StackTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+
+namespace LinkedLists.Tests
+{
+
+    [TestFixture]
+    public class StackTests
+    {
+        private MockStack<string>? stack;
+
+        [SetUp]
+        public void Setup()
+        {
+            stack = new MockStack<string>();
+        }
+
+        [Test]
+        public void Push_LengthIsZero_SetHeadToNodeAndIncrementLength()
+        {
+            stack.Push("Yasser");
+
+            Assert.AreEqual("Yasser", stack.Head!.value);
+            Assert.AreEqual(1, stack.length);
+        }
+
+        [Test]
+        public void Push_LengthIsNotZero_SetHeadNextAndHeadToNewNodeAndIncrementLength()
+        {
+            stack.Push("Yasser");
+            var oldHead = stack.Head;
+            stack.Push("Jack");
+            var newHead = stack.Head;
+
+            Assert.That(oldHead, Is.EqualTo(newHead.next));
+            Assert.That("Jack", Is.EqualTo(newHead.value));
+            Assert.AreEqual(2, stack.length);
+        }
+
+        [Test]
+        public void Pop_LengthIsZero_ReturnNull()
+        {
+            Assert.That(null, Is.EqualTo(stack.Pop()));
+        }
+
+        [Test]
+        public void Pop_LengthIsOne_SetHeadToNullAndLengthToZeroAndReturnPoppedValue()
+        {
+            stack.Push("Yasser");
+
+            string result = stack.Pop();
+
+            Assert.That(stack.Head, Is.Null);        
+            Assert.That(stack.length, Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo("Yasser"));
+        }
+
+        [Test]
+        public void Pop_LengthIsGreaterThanOne_DecrementLengthAndSetTheNewHeadToTheOldHeadNextRef()
+        {
+            stack.Push("Yasser");
+            stack.Push("Jack");
+            stack.Push("John");
+            stack.Push("Matt");
+            stack.Push("Christoph");
+
+            Node<string> oldHeadNextRef = stack.Head.next;
+            string result = stack.Pop();
+
+            Assert.That(stack.Head, Is.EqualTo(oldHeadNextRef));
+            Assert.That(stack.length, Is.EqualTo(4));
+            Assert.That(result, Is.EqualTo("Christoph"));
+        }
+
+        [Test]
+        public void Peek_IfLengthIsZero_ReturnTypeDefault()
+        {
+            Assert.That(stack.Peek(), Is.Null);
+        }
+
+        [Test]
+        public void Peek_IfLengthIsGreaterThanZero_ReturnStackHead()
+        {
+            stack.Push("Yasser");
+            stack.Push("Jack");
+            stack.Push("John");
+
+            Assert.That(stack.Peek(), Is.EqualTo("John"));
+        }
+
+    }
+
+
+    // A Mock Stack Class used to expose the Head for testing purposes
+    internal class MockStack<T> : Stack<T>
+    {
+
+        public Node<T>? Head
+        {
+            get { return head; }
+        }
+    }
+}
